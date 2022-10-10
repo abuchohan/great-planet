37ef165ba513705d98b3b54a05050a1af64aa3a5
Abu Chohan <abuchohan@users.noreply.github.com>
2022-10-10 12:38:48 +0100

    as

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Header from './components/Header/Header';
-import LoadingBar from './components/LoadingBar/LoadingBar';
 import Fullpage from './components/Fullpage/Fullpage';
 
 function App() {
   return (
 <>
 <Header />
-        {/* <LoadingBar /> */}
         <Fullpage />
         </>
   );
